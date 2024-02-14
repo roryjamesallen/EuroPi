d11e9eb2b68ed4eb34f031200b741e10db9a85d9
Rory Allen <79809962+roryjamesallen@users.noreply.github.com>
2024-02-14 17:07:50 +0000

    Create europi_extras.py

---
+++ software/firmware/europi_extras.py
@@ -0,0 +1,8 @@
+"""
+Rory Allen 2024 Apache License Version 2.0
+
+A collection of classes and functions which may be used in contrib scripts to prevent code replication or cross-dependent contrib scripts
+
+The europi_extras library is a single file named europi_extras.py. It should be imported into any custom program by using ``from europi_extras import *`` to give you full access to the functions within.
+"""
+
